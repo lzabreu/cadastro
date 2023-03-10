ae5414c576d25a15279ab7cd944da2cc82441f83
Luciano Abreu <lzabreu@mac.com>
2023-03-10 09:44:04 -0300

    conf de variaveis de Environment

--- src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
+++ src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Icon, Paper, TextField, useTheme } from '@mui/material'
 
+import { Environment } from '../../environment'
+
 interface IFerramentasDaListagemProps {
   textoDaBusca?: string
   mostrarInputBusca?: boolean
@@ -33,7 +35,7 @@ export const FerramentasDaListagem: React.FC<IFerramentasDaListagemProps> = ({
       {mostrarInputBusca && (
         <TextField
           size='small'
-          placeholder='Pesquisar...'
+          placeholder={Environment.INPUT_DE_BUSCA}
           value={textoDaBusca}
           onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
         />
--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,18 @@
-export {}
+export const Environment = {
+  /**
+   * Define a quantidade de linhas a ser carregada nas listagens
+   */
+  LIMITE_DE_LINHAS : 10,
+  /**
+   * Texto exibido no campo de busca
+   */
+  INPUT_DE_BUSCA: 'Pesquisar...',
+  /**
+   * Texto exibido quando não temos registros a serem exibidos
+   */
+  LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
+  /**
+   * Url base de consulta
+   */
+  URL_BASE: 'http://localhost.3333',
+}
